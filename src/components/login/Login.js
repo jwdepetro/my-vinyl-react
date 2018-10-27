f02682c61ab9c17472ecedb24fe91ba60c0fd15d
Jimmy <jwdepetro@macbooks-mbp.lan>
2018-10-27 15:32:47 -0600

    cleaned up material for login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import { Card, CardContent } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { withStyles, Button, CssBaseline, Paper, Avatar, Typography, FormControl, Input, InputLabel } from '@material-ui/core';
+import LockIcon from '@material-ui/icons/LockOutlined';
 import Axios from 'axios';
+
+const styles = theme => ({
+    layout: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+});
 
 class Login extends Component {
 
@@ -32,39 +62,54 @@ class Login extends Component {
     handleChange = name => event => this.setState({ [name]: event.target.value });
 
     render() {
+        const { classes } = this.props;
+
         return (
-            <Card>
-                <CardContent>
-                    <form onSubmit={(e) => this.login(e)}>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="username">Username</InputLabel>
-                            <Input
-                                id="username"
-                                name="username"
-                                autoComplete="username"
-                                onChange={this.handleChange('username')} autoFocus />
-                        </FormControl>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input
-                                id="password"
-                                name="password"
-                                type="password"
-                                autoComplete="current-password"
-                                onChange={this.handleChange('password')} />
-                        </FormControl>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="raised"
-                            color="primary">
-                            Login
-                    </Button>
-                    </form>
-                </CardContent>
-            </Card>
-        )
+            <React.Fragment>
+                <CssBaseline />
+                <main className={classes.layout}>
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <LockIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">Login</Typography>
+                        <form className={classes.form} onSubmit={(e) => this.login(e)}>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                    onChange={this.handleChange('username')} />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input
+                                    name="password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                    onChange={this.handleChange('password')} />
+                            </FormControl>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}>
+                                Login
+                            </Button>
+                        </form>
+                    </Paper>
+                </main>
+            </React.Fragment>
+        );
     }
 }
 
-export default Login;
+Login.propTypes = {
+    classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Login);
